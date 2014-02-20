921b218a1dc65bbdbd2992db75f5b62d867d07bc
Mark Meyer <mark.meyer37@gmail.com>
2014-02-19 21:44:30 -0600

    Initial Commit

---
+++ tictacsnow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tictacsnow
+//
+//  Created by Mark Meyer on 2/19/14.
+//  Copyright (c) 2014 Mark Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ tictacsnow/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  tictacsnow
+//
+//  Created by Mark Meyer on 2/19/14.
+//  Copyright (c) 2014 Mark Meyer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
